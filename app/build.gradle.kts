64f092516e11c9846c73a0c14711df9af3b1ed00
Tikhon <tihonbikbaev@gmail.com>
2024-07-23 23:22:52 +0300

    5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,9 @@ plugins {
 android {
     namespace = "com.example.firstkotlin3"
     compileSdk = 34
+    buildFeatures {
+        viewBinding = true
+    }
 
     defaultConfig {
         applicationId = "com.example.firstkotlin3"
